49156265286cc877668b3da619aabae683c5a988
xiangcitoZ <pzxiang1@gmail.com>
2023-05-24 14:21:04 +0200

    Add project files.

---
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApiPersonajesAWS.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcApiPersonajesAWS/Models/Personaje.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcApiPersonajesAWS.Models
+{
+    [Table("PERSONAJES")]
+    public class Personaje
+    {
+        [Key]
+        [Column("IDPERSONAJE")]
+        public int IdPersonaje { get; set; }
+        [Column("PERSONAJE")]
+        public string Nombre { get; set; }
+        [Column("IMAGEN")]
+        public string Imagen { get; set; }
+
+    }
+}
